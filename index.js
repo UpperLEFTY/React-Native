40648afce4c9f81cc11f52210377863019308ce5
Essence Alexander <upperlefty@zoho.eu>
2017-11-28 23:58:31 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class this is react native extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          If you like React is for the web. This is React Native for app development.
+        </Text>
+        <Text>
+           Using native components like 'View' and 'Text',
+          
+        </Text>
+      </View>
+    );
+  }
+}
